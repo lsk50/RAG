60d91be542d95337d9d5dbbf5931646033f949f5
lsk50 <114197910+lsk50@users.noreply.github.com>
2026-04-24 11:33:34 +0800

    Comment out protobuf installation and version checks
    Comment out protobuf installation and version info display

--- app_qa.py
+++ app_qa.py
@@ -3,11 +3,11 @@ import subprocess
 import sys
 import streamlit as st
 # 如果 protobuf 版本高于 4.0，强制降级到 3.20.3
-subprocess.check_call([sys.executable, "-m", "pip", "install", "protobuf==3.20.3"])
+# subprocess.check_call([sys.executable, "-m", "pip", "install", "protobuf==3.20.3"])
 
-st.write(f"当前 Python 版本: {sys.version}")
-st.write(f"Python 可执行文件路径: {sys.executable}")
-# ----------------------------------
+# st.write(f"当前 Python 版本: {sys.version}")
+# st.write(f"Python 可执行文件路径: {sys.executable}")
+# # ----------------------------------
 
 import time
 from rag import RagService
